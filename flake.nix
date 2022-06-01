a9dda14b4c4ef91730b5fdd0977eb66b3f47bfc2
Chris Pattison <chpattison@gmail.com>
2022-06-01 12:47:23 -0700

    minium reproducer

--- flake.nix
+++ flake.nix
@@ -8,29 +8,9 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # NUR packages
-    # nur.url = "github:nix-community/NUR";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      tempeh = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./tempeh-nixos/configuration.nix
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.chris = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-      };
-    };
-
     homeConfigurations.chris = home-manager.lib.homeManagerConfiguration {
       system = "x86_64-linux";
       configuration = import ./home.nix;
--- home.nix
+++ home.nix
@@ -1,10 +1,6 @@
 { pkgs, ...}:
 {
 
-  imports = [
-    #nur-no-pkgs.repos.rycee.hmMOdules.emacs-init
-  ];
-
   nixpkgs.config.allowUnfree = true;
 
   home.username = "chris";
@@ -13,49 +9,8 @@
   home.stateVersion = "22.05";
   programs.home-manager.enable = true;
 
-  home.packages = let
-    texlive = (pkgs.texlive.combine { inherit (pkgs.texlive) 
-            scheme-medium collection-fontsextra adjustbox cleveref 
-            makecell enumitem collectbox thmtools braket relsize preprint
-            standalone quantikz xargs xstring environ tikz-cd pythontex revtex; });
-    python-with-pkgs = (pkgs.python3.withPackages (python-packages: with python-packages; [
-        pandas
-        numpy
-        scipy
-        matplotlib
-		    pytest
-		    networkx
-      ]));
-  in [
-    pkgs.htop
-    pkgs.libfido2
-    texlive
-    python-with-pkgs
+  home.packages = [
     pkgs.slack
   ];
 
-  programs.firefox = {
-    enable = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    defaultKeymap = "emacs";
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "Chris Pattison";
-    userEmail = "chpattison@gmail.com";
-  };
-
-  programs.zathura = {
-    enable = true;
-    extraConfig = builtins.readFile (./. + "/rc/emacs-key-for-zathura.txt");
-  };
-
-  programs.emacs = {
-    enable = true;
-    extraConfig = builtins.readFile (./. + "/rc/init.el");
-  };
 }
