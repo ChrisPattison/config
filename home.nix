2cdc69def2b62cd7437872286e0fb9b4343dd235
Chris Pattison <chpattison@gmail.com>
2022-06-06 14:13:28 -0700

    Add email account

--- home.nix
+++ home.nix
@@ -60,4 +60,16 @@
     enable = true;
     extraConfig = builtins.readFile (./. + "/rc/init.el");
   };
+
+  accounts.email = {
+    accounts.outlook = {
+      primary = true;
+      realName = "Chris Pattison";
+      address = "cpattiso@caltech.edu";
+
+      imap.host = "outlook.office365.com";
+      smtp.host = "smtp.office365.com";
+      userName = "cpattiso@caltech.edu";
+    };
+  };
 }
