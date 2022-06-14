0b12029be5ff0fb3a9c1f37c7f3592f2297e04e0
Chris Pattison <chpattison@gmail.com>
2022-06-14 10:57:22 -0700

    Fix texlive packages whitespace

--- home.nix
+++ home.nix
@@ -17,9 +17,9 @@
 
   home.packages = let
     texlive = (pkgs.texlive.combine { inherit (pkgs.texlive) 
-            scheme-medium collection-fontsextra adjustbox cleveref 
-            makecell enumitem collectbox thmtools braket relsize preprint
-            standalone quantikz xargs xstring environ tikz-cd pythontex revtex; });
+      scheme-medium collection-fontsextra adjustbox cleveref 
+      makecell enumitem collectbox thmtools braket relsize preprint
+      standalone quantikz xargs xstring environ tikz-cd pythontex revtex; });
     python-with-pkgs = (pkgs.python3.withPackages (python-packages: with python-packages; [
         pandas
         numpy
