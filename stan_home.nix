b740b9b88c6cf77bea5c93e6be3f3a03d79890cf
Chris Pattison <chpattison@gmail.com>
2022-06-14 13:45:20 -0700

    Remove VSCode, add NodeJS to stan

--- stan_home.nix
+++ stan_home.nix
@@ -17,26 +17,13 @@
         pytest
         networkx
     ]));
-    vscode-with-extensions = pkgs.vscode-with-extensions.override {
-      vscodeExtensions = (with pkgs.vscode-extensions; [
-        bbenoist.nix
-        ms-python.python
-        ms-azuretools.vscode-docker
-        ms-vscode-remote.remote-ssh
-      ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
-        name = "remote-ssh-edit";
-        publisher = "ms-vscode-remote";
-        version = "0.47.2";
-        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      }];
-    }; 
   in [
     pkgs.htop
     python-with-pkgs
     pkgs.cmake
     pkgs.gcc
     pkgs.git
-    vscode-with-extensions
+    pkgs.nodejs
   ];
 
   programs.zsh = {
