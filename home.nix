67f2ce3f22bd168fc9d552885c87adb7c64b434d
Chris Pattison <chpattison@gmail.com>
2022-10-05 14:40:40 -0700

    Add fcitx5

--- home.nix
+++ home.nix
@@ -51,6 +51,13 @@
     ];
   };
 
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+    ];
+  };
+
   programs.zsh = {
     enable = true;
     defaultKeymap = "emacs";
