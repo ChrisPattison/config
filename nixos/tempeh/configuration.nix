76f1db26c4c83eb5a49bd590a71b375eaf8b6bbe
Chris Pattison <chpattison@gmail.com>
2023-05-30 12:36:16 -0700

    Remove boot.loader.grub.version option

--- nixos/tempeh/configuration.nix
+++ nixos/tempeh/configuration.nix
@@ -10,7 +10,6 @@
   
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = false;
-  boot.loader.grub.version = 2;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
