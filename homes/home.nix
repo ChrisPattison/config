bd8d11770252b3cd7db020ff385da6b63ed30eee
Chris Pattison <chpattison@gmail.com>
2023-07-05 12:07:30 -0700

    Revert "Add OBS"
    This reverts commit 1f2c02d9b6d6ec69a3eacb412a89a9c6298820c6.

--- homes/home.nix
+++ homes/home.nix
@@ -87,14 +87,5 @@
       "open" = "xdg-open";
     };
   };
-
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-pipewire-audio-capture
-      obs-vkcapture
-    ];
-  };
 }
 
