8c12da67d7ae4a0060739050cd9f26f91e27c786
Chris Pattison <chpattison@gmail.com>
2024-02-10 14:56:21 -0800

    Add inkscape

--- homes/darwin-home.nix
+++ homes/darwin-home.nix
@@ -9,7 +9,7 @@
   home.username = "chris";
   home.homeDirectory = "/Users/chris";
   
-  home.packages = [ pkgs.home-manager pkgs.htop pkgs.texlive.combined.scheme-medium pkgs.ghostscript ];
+  home.packages = [ pkgs.home-manager pkgs.htop pkgs.texlive.combined.scheme-full pkgs.ghostscript pkgs.lean4 pkgs.typst pkgs.typst-lsp pkgs.cargo pkgs.rustc pkgs.inkscape];
 
   nix.package = pkgs.nix;
   nix.settings = {
