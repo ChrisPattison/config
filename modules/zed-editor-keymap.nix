734a49f93df7b7baafe717f07b8aac30e3c68821
Chris Pattison <chpattison@gmail.com>
2024-11-28 17:35:14 -0800

    Add to keymap

--- modules/zed-editor-keymap.nix
+++ modules/zed-editor-keymap.nix
@@ -1,6 +1,15 @@
 [
 {
-  context = "Workspace";
+  context = "Editor";
+  bindings = {
+    "ctrl-x" = null;
+    "ctrl-c" = null;
+    "ctrl-v" = null;
+    "ctrl-;" = null;
+  };
+}
+{
+  context = "Editor";
   bindings = {
     "ctrl-x ;" = ["editor::ToggleComments" { "advance_downwards" = false; }];
   };
@@ -8,7 +17,6 @@
 {
   context = "Editor";
   bindings = {
-    "ctrl-;" = null;
     "ctrl-/" = "editor::Undo";
     "ctrl-k" = "editor::DeleteLine";
     "ctrl-w" = "editor::Cut";
